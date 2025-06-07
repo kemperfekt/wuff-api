726ed56d7e6c193d1f1b63a9766d466350632864
Kemperfekt <pk@kemperfekt.de>
2025-06-07 15:25:47 +0200

    Correct app names to wuff-api and wuff-web for Scalingo rename

--- src/main.py
+++ src/main.py
@@ -301,8 +301,8 @@ production_origins = [
     "https://api.wuffchat.de",
     "https://dogbot-agent.osc-fr1.scalingo.io",
     "https://dogbot-ui.osc-fr1.scalingo.io",
-    "https://wuffchat-api.osc-fr1.scalingo.io",
-    "https://wuffchat-web.osc-fr1.scalingo.io",
+    "https://wuff-api.osc-fr1.scalingo.io",
+    "https://wuff-web.osc-fr1.scalingo.io",
 ]
 
 development_origins = [
